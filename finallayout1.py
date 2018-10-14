3c7764965dedaeadf74e9d4520cb1925ad4a87ae
PavanKumarDharmoju <pavankumardharmoju@gmail.com>
2018-10-14 07:23:39 +0530

    the final layout with time

--- finallayout1.py
+++ finallayout1.py
@@ -1,23 +1,28 @@
 from tkinter import *
-
+import time;
 root=Tk()
-i=1	
-topF=Frame(root,width=40)
-bottomF=Frame(root,width=40)
+root.title("Message Commute")
+root.geometry("400x600")
+i=1
+localtime = time.asctime( time.localtime(time.time()) )
+topF=Frame(root,width=160,bg="black")
+bottomF=Frame(root,width=160)
 bottomF.pack(side=BOTTOM)
 topF.pack()
 def fun():
-	global i
-	sampleL2=Label(topF,text=E1.get(),fg="blue")
-	sampleL2.grid(row=i,column=1,sticky=E)
-	i=i+1
-	E1.delete(0,END)
+        global i
+        s.append(E1.get())
+        sampleL2=Label(topF,text="ME:" + E1.get(),fg="blue")
+        sampleL2.grid(row=i,column=1,sticky=E)
+        i=i+1
+        E1.delete(0,END)
 E1=Entry(bottomF)
 E1.grid(row=0)
 B=Button(bottomF,text="Send",bg="green",command=fun)
-B.grid(row=0,column=1)
-sampleL1=Label(topF,text="sampleL1",fg="green")
+B.grid(row=0,column=1,sticky=E)
+sampleL1=Label(topF,text=localtime,fg="green")
 sampleL1.grid(row=0,sticky=W)
 
 
 root.mainloop()
+
